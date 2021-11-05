b003b8516fa73973584621970db2b39b3934226e
sagajdak <polina.sagaydak@mail.ru>
2021-11-05 20:15:41 +0300

    add 005 If_Else

---
+++ 005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Это же Избранная!!!");
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}
